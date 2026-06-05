255d00bbd3b49169882b70368634c95ebd4a26c3
Hunter Liles <hunterliles06@gmail.com>
2026-06-05 16:56:24 -0500

    Added player animation system of all 8 directions. Moved all movement logich into conditional statements.

--- src/main.c
+++ src/main.c
@@ -1,75 +1,62 @@
 #include "raylib.h"
 #include "raymath.h"
 
-#define NUM_FRAMES 6
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 
+const int NUM_ROWS = 8;
+const int PIXELS = 32;
+
+typedef enum { IDLE = 0, WALK, RUN, ATTACK, NUM_ANIM } AnimState;
+
 typedef enum {
-  IDLE_RIGHT = 0,
-  IDLE_LEFT,
-  IDLE_UP,
-  IDLE_DOWN,
-  RUN_RIGHT,
-  RUN_LEFT,
-  RUN_UP,
-  RUN_DOWN,
-  NUM_ANIM
-} AnimState;
+  NW = 0,
+  W,
+  SW,
+  S,
+  SE,
+  E,
+  NE,
+  N,
+  NUM_DIR
+} Direction; // Get attack to have the correct directions.
 
 typedef struct {
   Vector2 pos;
   Rectangle rec;
   Texture2D tex[NUM_ANIM];
   AnimState currState;
-  bool isMoving;
+  Direction currDir;
 } player;
 
-void player_anim(player *p, int numFrames, int *currFrame, int *frameCounter,
-                 int frameSpeed);
-void player_move(player *p, float speed, float sprint);
+player p;
+
+void player_anim(player *p, int *currFrame, int *frameCounter, int frameSpeed);
+Vector2 player_move(player *p, float speed, float sprint);
+void player_init(player *p);
 
 int main(void) {
-  ChangeDirectory(TextFormat(
-      "%s/..", GetApplicationDirectory())); // Make sure the working directory
-                                            // is set in blossom_c folder.
+  ChangeDirectory(TextFormat("%s/..", GetApplicationDirectory()));
   InitWindow(1280, 720, "Blossom");
   GuiLoadStyle("styles/style_dark.rgs");
+  Texture2D background = LoadTexture("assets/nature/nature_4/origbig.png");
 
-  Shader shader = LoadShader(0, "shaders/shader.frag");
+  Texture2D enviroment[3] = {
+      LoadTexture(""), LoadTexture(""),
+      LoadTexture("")}; // Is this the best way to do this?
 
-  player p = {.pos = (Vector2){(float)GetScreenWidth() / 2.0f,
-                               (float)GetScreenHeight() / 2.0f},
-              .currState = IDLE_DOWN,
-              .isMoving = false};
-
-  p.tex[IDLE_DOWN] = LoadTexture("assets/Idle_Down.png");
-  p.tex[IDLE_UP] = LoadTexture("assets/Idle_Up.png");
-  p.tex[IDLE_LEFT] = LoadTexture("assets/Idle_Left.png");
-  p.tex[IDLE_RIGHT] = LoadTexture("assets/Idle_Right.png");
-  p.tex[RUN_LEFT] = LoadTexture("assets/Run_Left.png");
-  p.tex[RUN_RIGHT] = LoadTexture("assets/Run_Right.png");
-  p.tex[RUN_UP] = LoadTexture("assets/Run_Up.png");
-  p.tex[RUN_DOWN] = LoadTexture("assets/Run_Down.png");
-  p.rec = (Rectangle){0, 8, (float)p.tex[p.currState].width / NUM_FRAMES,
-                      (float)p.tex[p.currState].height};
+  player_init(&p);
 
   int currFrame = 0;
   int frameCounter = 0;
-
-  float resolution[2] = {GetScreenWidth(), GetScreenHeight()};
 
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
 
-    float time = GetTime();
-    SetShaderValue(shader, GetShaderLocation(shader, "time"), &time,
-                   SHADER_UNIFORM_FLOAT);
-
     // Everything that isn't a texture
-    player_anim(&p, NUM_FRAMES, &currFrame, &frameCounter, 8);
-    player_move(&p, 3.0f, 50.0f);
+    player_anim(&p, &currFrame, &frameCounter, 8);
+    p.pos = Vector2Add(p.pos, player_move(&p, 3.0f, 50.0f));
 
     BeginDrawing();
 
@@ -77,8 +64,8 @@ int main(void) {
 
     DrawText("Alpha", (float)GetScreenWidth() / 2.0f - 48.0f,
              (float)GetScreenHeight() / 2.0f - 60.0f, 48, GRAY);
+    DrawTexture(background, 0, 0, WHITE);
 
-    // BeginShaderMode(shader);
     //  Static textures
     DrawRectanglePro(
         (Rectangle){0, 0, GetScreenWidth(), GetScreenHeight() / 2.0f},
@@ -86,8 +73,6 @@ int main(void) {
     DrawRectangleLinesEx(
         (Rectangle){0, 0, GetScreenWidth(), GetScreenHeight() / 2.0f}, 5.0f,
         BLACK);
-
-    // EndShaderMode();
 
     // Dynamic textures
     DrawTextureRec(p.tex[p.currState], p.rec, p.pos, WHITE); // player
@@ -102,60 +87,86 @@ int main(void) {
   for (int i = 0; i < NUM_ANIM; i++)
     UnloadTexture(p.tex[i]);
 
-  UnloadShader(shader);
   CloseWindow();
   return 0;
 }
 
-void player_anim(player *p, int numFrames, int *currFrame, int *frameCounter,
-                 int frameSpeed) {
+void player_init(player *p) {
+  p->pos = (Vector2){(float)GetScreenWidth() / 2.0f,
+                     (float)GetScreenHeight() / 2.0f};
+  p->currState = IDLE;
+  p->currDir = S;
+  p->tex[IDLE] = LoadTexture("assets/player-anim/idle.png");
+  p->tex[WALK] = LoadTexture("assets/player-anim/walk.png");
+  p->tex[RUN] = LoadTexture("assets/player-anim/run.png");
+  p->tex[ATTACK] = LoadTexture("assets/player-anim/attack.png");
+  p->rec = (Rectangle){0, 0, PIXELS, PIXELS};
+}
+
+void player_anim(player *p, int *currFrame, int *frameCounter, int frameSpeed) {
+
   (*frameCounter)++;
 
   if (*frameCounter >= (60 / frameSpeed)) {
     *frameCounter = 0;
     (*currFrame)++;
 
-    if (*currFrame >= numFrames)
+    if (*currFrame >= (p->tex[p->currState].width / PIXELS))
       *currFrame = 0;
 
-    p->rec.x =
-        (float)*currFrame * (float)p->tex[p->currState].width / numFrames;
+    p->rec.x = (float)(*currFrame * p->tex[p->currState].width /
+                       (p->tex[p->currState].width / (float)PIXELS));
+    p->rec.y =
+        (float)(p->currDir * p->tex[p->currState].height / (float)NUM_ROWS);
   }
 }
 
-void player_move(player *p, float speed, float sprint) {
+Vector2 player_move(player *p, float speed, float sprint) {
   Vector2 move = Vector2Zero();
+  p->pos.y =
+      Clamp(p->pos.y, GetScreenHeight() / 2.0f, GetScreenHeight() - PIXELS);
+  p->pos.x = Clamp(p->pos.x, 0.0f, GetScreenWidth() - PIXELS);
 
-  if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
-    move.x -= speed;
-    if (!(p->currState == RUN_LEFT)) {
-      p->currState = RUN_LEFT;
-      p->isMoving = true;
-    }
-  }
-  if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
-    move.x += speed;
-    if (!(p->currState == RUN_RIGHT)) {
-      p->currState = RUN_RIGHT;
-      p->isMoving = true;
-    }
-  }
-  if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) {
-    move.y -= speed;
-    if (!(p->currState == RUN_UP)) {
-      p->currState = RUN_UP;
-      p->isMoving = true;
-    }
-  }
-  if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) {
-    move.y += speed;
-    if (!(p->currState == RUN_DOWN)) {
-      p->currState = RUN_DOWN;
-      p->isMoving = true;
-    }
-  }
-  if (IsKeyPressed(KEY_LEFT_SHIFT))
-    move = Vector2Scale(move, sprint);
+  move.x = (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) ? move.x - speed : move.x;
+  move.x = (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) ? move.x + speed : move.x;
+  move.y = (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) ? move.y - speed : move.y;
+  move.y = (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) ? move.y + speed : move.y;
+  move = IsKeyPressed(KEY_LEFT_SHIFT) ? Vector2Scale(move, sprint)
+                                      : move; // turn this back into a sprint.
 
-  p->pos = Vector2Add(p->pos, move);
+  p->currState = (move.x == 0 && move.y == 0) ? IDLE : p->currState;
+  if (move.x > 0) {
+    p->currState = WALK;
+    p->currDir = E;
+  }
+  if (move.x < 0) {
+    p->currState = WALK;
+    p->currDir = W;
+  }
+  if (move.y > 0) {
+    p->currState = WALK;
+    p->currDir = S;
+  }
+  if (move.y < 0) {
+    p->currState = WALK;
+    p->currDir = N;
+  }
+  if (move.x > 0 && move.y > 0) {
+    p->currState = WALK;
+    p->currDir = SE;
+  }
+  if (move.x < 0 && move.y > 0) {
+    p->currState = WALK;
+    p->currDir = SW;
+  }
+  if (move.x > 0 && move.y < 0) {
+    p->currState = WALK;
+    p->currDir = NE;
+  }
+  if (move.x < 0 && move.y < 0) {
+    p->currState = WALK;
+    p->currDir = NW;
+  }
+
+  return move;
 }

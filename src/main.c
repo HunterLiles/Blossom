bc637f045756e135b7a247c0c2535e4316be0c11
Hunter Liles <hunterliles06@gmail.com>
2026-06-16 16:49:48 -0500

    Added a camera that can zoom in and out. Added offsets to the textures to line up with the tiles. Added delta time to all frame dependent things.

--- src/main.c
+++ src/main.c
@@ -4,16 +4,11 @@
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 
-#define SWIDTH 1920
-#define SHEIGHT 1080
-#define NUM_ROWS 8
-#define PIXELS 32
-#define MAX_ENV 25
-#define MAX_NPC 25
+#define TILESIZE 32
 
 typedef enum { IDLE = 0, WALK, RUN, ATTACK, NUM_ANIM } AnimState;
 typedef enum { NW = 0, W, SW, S, SE, E, NE, N, NUM_DIR } Direction;
-typedef enum { GRASS = 0, TREE, GTREE, LEAVES, LTREE, GLTREE, NUM_ENV } EnvType;
+typedef enum { NONE = 0, GRASS, TREE, GTREE, LEAVES, NUM_ENV } EnvType;
 
 typedef struct {
   Vector2 pos;
@@ -21,13 +16,10 @@ typedef struct {
   Texture2D tex[NUM_ANIM];
   AnimState currState;
   Direction currDir;
+  int currFrame;
+  int frameCounter;
+  char name[10];
 } player;
-
-typedef struct {
-  Vector2 pos;
-  Rectangle rec;
-  Texture2D tex;
-} NPC;
 
 typedef struct {
   Vector2 pos;
@@ -39,98 +31,133 @@ typedef struct {
   int layer1, layer2;
 } Grid;
 
-player p;
-
-void player_anim(player *p, int *currFrame, int *frameCounter, int frameSpeed);
+void player_anim(player *p, int frameSpeed);
 Vector2 player_move(player *p, float speed, float sprint, float dash);
 
 int main(void) {
   ChangeDirectory(TextFormat("%s/..", GetApplicationDirectory()));
-  InitWindow(SWIDTH, SHEIGHT, "Blossom");
+  InitWindow(1920, 1080, "Blossom");
   GuiLoadStyle("styles/style_dark.rgs");
 
   // Player Init
-  p.pos = (Vector2){(float)GetScreenWidth() / 2.0f,
-                    (float)GetScreenHeight() / 2.0f};
-  p.currState = IDLE;
-  p.currDir = S;
-  p.tex[IDLE] = LoadTexture("assets/player-anim/idle.png");
-  p.tex[WALK] = LoadTexture("assets/player-anim/walk.png");
-  p.tex[RUN] = LoadTexture("assets/player-anim/run.png");
-  p.tex[ATTACK] = LoadTexture("assets/player-anim/attack.png");
-  p.rec = (Rectangle){0, 0, PIXELS, PIXELS};
 
-  // Tilemap setup
+  player p = {
+      .pos = {(float)GetScreenWidth() / 2.0f, (float)GetScreenHeight() / 2.0f},
+      .currState = IDLE,
+      .currDir = S,
+      .tex[IDLE] = LoadTexture("assets/player-anim/idle.png"),
+      .tex[WALK] = LoadTexture("assets/player-anim/walk.png"),
+      .tex[RUN] = LoadTexture("assets/player-anim/run.png"),
+      .tex[ATTACK] = LoadTexture("assets/player-anim/attack.png"),
+      .rec = {0, 0, 32, 32},
+      .name = "Hunter",
+  };
+
+  player npc[10] = {
+      {
+          .pos = {500.0f, 500.0f},
+          .currState = IDLE,
+          .currDir = S,
+          .tex[IDLE] = LoadTexture("assets/player-anim/idle.png"),
+          .tex[WALK] = LoadTexture("assets/player-anim/walk.png"),
+          .tex[RUN] = LoadTexture("assets/player-anim/run.png"),
+          .tex[ATTACK] = LoadTexture("assets/player-anim/attack.png"),
+          .rec = {0, 0, 32, 32},
+          .name = "Gary",
+      },
+  };
+
+  // TODO : Create switch for different levels and have an array for each level.
+  // Combine the two grids somehow for the level array?
   Grid back_grid[512][512];
   Grid fore_grid[512][512];
 
   Texture2D envTex = LoadTexture("assets/trees.png");
-  Environment env[MAX_ENV] = {
-      {.tex = envTex, .rec = (Rectangle){352, 576, PIXELS, PIXELS}},
-      {.tex = envTex, .rec = (Rectangle){224, 490, 36, 54}},
+  Environment env[10] = {
+      {},
+      {.tex = envTex, .rec = (Rectangle){352, 576, 32, 32}},
+      {.tex = envTex, .rec = (Rectangle){224, 305, 176, 176}},
       {.tex = envTex, .rec = (Rectangle){272, 490, 36, 54}},
-      {.tex = envTex, .rec = (Rectangle){736, 752, PIXELS, PIXELS}}};
+      {.tex = envTex, .rec = (Rectangle){736, 752, 32, 32}}};
 
-  NPC npc[MAX_NPC] = {
-      {(Vector2){250, 250}, (Rectangle){0, 96, 32, 32}, p.tex[IDLE]}};
-
-  for (int i = 0; i < 512; i++) {
-    for (int j = 0; j < 512; j++) {
-      back_grid[i][j] = (i % 5 == 0 && j % 5 == 0) ? (Grid){GRASS, LEAVES}
-                                                   : (Grid){GRASS, -1};
-      fore_grid[i][j] =
-          (i % 5 == 0 && j % 5 == 0) ? (Grid){TREE, -1} : (Grid){-1, -1};
+  for (int i = 0; i < 256; i++) {
+    for (int j = 0; j < 256; j++) {
+      back_grid[i][j] = (i % 5 == 0 && j % 5 == 0) ? (Grid){NONE, LEAVES}
+                                                   : (Grid){NONE, NONE};
+      fore_grid[i][j] = (i % 10 == 0 && j % 10 == 0) ? (Grid){TREE, NONE}
+                                                     : (Grid){NONE, NONE};
     }
   }
 
-  int currFrame = 0;
-  int frameCounter = 0;
+  // TODO : Get the offset correct.
+  Camera2D cam = {(Vector2){p.pos.x - p.rec.width, p.pos.y - p.rec.height},
+                  (Vector2){GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f},
+                  0.0f, 1.0f};
 
-  SetTargetFPS(60);
+  SetTargetFPS(180);
 
   while (!WindowShouldClose()) {
 
-    // Everything that isn't a texture
-    player_anim(&p, &currFrame, &frameCounter, 8);
-    p.pos = Vector2Add(p.pos, player_move(&p, 3.0f, 1.5f, 50.0f));
+    // NOTE : Pre-rendering things
+    player_anim(&p, 8);
+    player_anim(&npc[0], 8);
+    p.pos = Vector2Add(p.pos, player_move(&p, 100.0f, 1.5f, 1000.0f));
+
+    cam.target = (Vector2){p.pos.x - (p.rec.width / 2.0f),
+                           p.pos.y - (p.rec.height / 2.0f)};
+    cam.zoom = expf(logf(cam.zoom) + ((float)GetMouseWheelMove() * 0.1f));
+    if (cam.zoom > 3.0f)
+      cam.zoom = 3.0f;
+    else if (cam.zoom < 1.0f)
+      cam.zoom = 1.0f;
 
     BeginDrawing();
+    ClearBackground(BLACK);
+    BeginMode2D(cam);
 
-    ClearBackground(RAYWHITE);
-
-    //  Background textures
+    // NOTE : Background textures
     for (int i = 0; i < 64; i++) {
-      for (int j = 0; j < 64; j++) {
-        DrawTextureRec(
-            env[back_grid[i][j].layer1].tex, env[back_grid[i][j].layer1].rec,
-            (Vector2){(float)(i * PIXELS), (float)(j * PIXELS)}, WHITE);
-        DrawTextureRec(
-            env[back_grid[i][j].layer2].tex, env[back_grid[i][j].layer2].rec,
-            (Vector2){(float)(i * PIXELS), (float)(j * PIXELS)}, WHITE);
+      for (int j = 0; j < 40; j++) {
+        DrawTextureRec(env[GRASS].tex, env[GRASS].rec,
+                       (Vector2){(float)(i * TILESIZE), (float)(j * TILESIZE)},
+                       WHITE);
+        if (back_grid[i][j].layer2 == LEAVES)
+          DrawTextureRec(
+              env[LEAVES].tex, env[LEAVES].rec,
+              (Vector2){(float)(i * TILESIZE), (float)(j * TILESIZE)}, WHITE);
       }
     }
 
+    // NOTE : Animated textures
+    DrawTextureRec(npc[0].tex[npc[0].currState], npc[0].rec, npc[0].pos,
+                   WHITE);                                   // NPC
+    DrawTextureRec(p.tex[p.currState], p.rec, p.pos, WHITE); // player
+
+    // NOTE : Foreground textures
+    for (int i = 0; i < 64; i++) {
+      for (int j = 0; j < 40; j++) {
+        if (fore_grid[i][j].layer1 == TREE)
+          DrawTextureRec(
+              env[TREE].tex, env[TREE].rec,
+              (Vector2){(float)(i * TILESIZE) - (env[TREE].rec.width / 2.0f),
+                        (float)(j * TILESIZE) - (env[TREE].rec.height)},
+              WHITE);
+      }
+    }
+
+    // NOTE : Dynamic UI
+    DrawRectangle(npc[0].pos.x - 5, npc[0].pos.y - 20, 40, 20, WHITE);
+    DrawText(npc[0].name, npc[0].pos.x, npc[0].pos.y - 15, 12, BLACK);
+
+    EndMode2D();
+
+    // NOTE : UI.
     DrawText("Alpha", (float)GetScreenWidth() - 80.0f, 0, 24, GRAY);
 
-    // Dynamic textures
-    DrawTextureRec(npc[0].tex, npc[0].rec, npc[0].pos, WHITE); // NPC
-    DrawTextureRec(p.tex[p.currState], p.rec, p.pos, WHITE);   // player
-
-    // Foreground textures
-    for (int i = 0; i < 64; i++) {
-      for (int j = 0; j < 64; j++) {
-        DrawTextureRec(
-            env[fore_grid[i][j].layer1].tex, env[fore_grid[i][j].layer1].rec,
-            (Vector2){(float)(i * PIXELS), (float)(j * PIXELS) - PIXELS},
-            WHITE);
-        DrawRectangleLines((float)(i * PIXELS), (float)(j * PIXELS), PIXELS,
-                           PIXELS, GRAY);
-      }
-    }
-
-    // UI.
-    GuiPanel((Rectangle){10, 10, 65, 50}, "Settings");
-    GuiLabel((Rectangle){15, 50, 40, 1}, TextFormat("FPS: %i", GetFPS()));
+    GuiPanel((Rectangle){10, 10, 200, 100}, "Settings");
+    GuiLabel((Rectangle){15, 50, 400, 1}, TextFormat("FPS: %i", GetFPS()));
+    GuiLabel((Rectangle){15, 70, 150, 1},
+             TextFormat("x: %.2f, y: %.2f", p.pos.x, p.pos.y));
 
     EndDrawing();
   }
@@ -144,29 +171,31 @@ int main(void) {
   return 0;
 }
 
-void player_anim(player *p, int *currFrame, int *frameCounter, int frameSpeed) {
+void player_anim(player *p, int frameSpeed) {
 
-  (*frameCounter)++;
+  p->frameCounter++;
 
-  if (*frameCounter >= (60 / frameSpeed)) {
-    *frameCounter = 0;
-    (*currFrame)++;
+  if (p->frameCounter >= ((1 / GetFrameTime()) / frameSpeed)) {
+    p->frameCounter = 0;
+    p->currFrame++;
 
-    if (*currFrame >= (p->tex[p->currState].width / PIXELS))
-      *currFrame = 0;
+    if (p->currFrame >= (p->tex[p->currState].width / 32))
+      p->currFrame = 0;
 
-    p->rec.x = (float)(*currFrame * p->tex[p->currState].width /
-                       (p->tex[p->currState].width / (float)PIXELS));
-    p->rec.y =
-        (float)(p->currDir * p->tex[p->currState].height / (float)NUM_ROWS);
+    p->rec.x = (float)(p->currFrame * p->tex[p->currState].width /
+                       (p->tex[p->currState].width / (float)32));
+    p->rec.y = (float)(p->currDir * p->tex[p->currState].height / 8.0f);
   }
 }
 
 Vector2 player_move(player *p, float speed, float sprint, float dash) {
   Vector2 move = Vector2Zero();
-  p->pos.y = Clamp(p->pos.y, 0.0f, GetScreenHeight() - PIXELS);
-  p->pos.x = Clamp(p->pos.x, 0.0f, GetScreenWidth() - PIXELS);
+  // TODO : Make a struct for maps and store the number of tiles in each map so
+  // the max width and height of each map is easily gotten.
+  p->pos.y = Clamp(p->pos.y, 0.0f, GetScreenHeight() - p->rec.width);
+  p->pos.x = Clamp(p->pos.x, 0.0f, GetScreenWidth() - p->rec.height);
 
+  speed *= GetFrameTime();
   speed = (IsKeyDown(KEY_LEFT_SHIFT)) ? speed * sprint : speed;
   move.x = (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) ? move.x - speed : move.x;
   move.x = (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) ? move.x + speed : move.x;
